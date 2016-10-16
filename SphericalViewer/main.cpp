65a290db31ecd6b382fb38c03b8b92243c17a938
humek <uselym@gmail.com>
2016-10-16 21:46:04 +0800

    删除测试图片

--- SphericalViewer/main.cpp
+++ SphericalViewer/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glHelper* window = new glHelper(1300, 1000, 10, 10);
-	string filename = "img/3.bmp";
+	string filename = "img/1.bmp";
 	string title = "Spherical Viewer";
 	window->init(filename.c_str(), title.c_str());
 	window->run();
